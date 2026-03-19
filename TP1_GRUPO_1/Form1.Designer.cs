a18b7c67932d0077769f2a72fd10785dd46fbcac
Etherina <senageremias20@gmail.com>
2026-03-18 21:05:27 -0300

    Gabi añadio el label de la lista de participantes del grupo

--- TP1_GRUPO_1/Form1.Designer.cs
+++ TP1_GRUPO_1/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.btnEjerUno = new System.Windows.Forms.Button();
             this.btnEjerDos = new System.Windows.Forms.Button();
             this.btnEjerTres = new System.Windows.Forms.Button();
+            this.Integrantes1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnEjerUno
@@ -60,17 +61,28 @@
             this.btnEjerTres.Text = "EJERCICIO 3\r\n";
             this.btnEjerTres.UseVisualStyleBackColor = true;
             // 
+            // Integrantes1
+            // 
+            this.Integrantes1.Location = new System.Drawing.Point(40, 170);
+            this.Integrantes1.Name = "Integrantes1";
+            this.Integrantes1.Size = new System.Drawing.Size(100, 23);
+            this.Integrantes1.TabIndex = 3;
+            this.Integrantes1.Text = "Integrantes:";
+            this.Integrantes1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.Integrantes1);
             this.Controls.Add(this.btnEjerTres);
             this.Controls.Add(this.btnEjerDos);
             this.Controls.Add(this.btnEjerUno);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Formulario Principal";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -80,6 +92,7 @@
         private System.Windows.Forms.Button btnEjerUno;
         private System.Windows.Forms.Button btnEjerDos;
         private System.Windows.Forms.Button btnEjerTres;
+        private System.Windows.Forms.Label Integrantes1;
     }
 }
 
